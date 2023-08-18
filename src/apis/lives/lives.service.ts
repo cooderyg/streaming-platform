3af1c9719377986ebc894401df69952a3133ac4f
issuebombom <issuebombom@gmail.com>
2023-08-18 12:53:33 +0900

    feat: live update & findById API 방송 정보 업데이트 기능 구현 - 현재 title, tag 변경에 대한 업데이트 구현 - 추후 방송 종료 시 업데이할 기능 구현 필요

--- src/apis/lives/lives.service.ts
+++ src/apis/lives/lives.service.ts
@@ -6,6 +6,7 @@ import { ChannelsService } from 'src/apis/channels/channels.service';
 import { Live } from './entities/live.entity';
 import { TagsService } from '../tags/tags.service';
 import { CreateTagDto } from '../tags/dto/create-tag.dto';
+import { UpdateLiveDto } from './dto/update-live.dto';
 
 @Injectable()
 export class LivesService {
@@ -15,6 +16,14 @@ export class LivesService {
     private readonly channelsService: ChannelsService,
     private readonly tagsService: TagsService,
   ) {}
+
+  async getLiveById(liveId: string) {
+    return await this.livesRepository
+      .createQueryBuilder('live')
+      .leftJoinAndSelect('live.tags', 'tag')
+      .where('live.id = :id', { id: liveId })
+      .getOne();
+  }
 
   async createLive({ userId, createLiveDto }: ILivesServiceCreateLive) {
     const { title, ...createTagDto } = createLiveDto;
@@ -27,9 +36,28 @@ export class LivesService {
     });
     return live;
   }
+
+  async updateLive({ liveId, updateLiveDto }: ILivesServiceUpdateLive) {
+    const { title, ...createTagDto } = updateLiveDto;
+    const live = await this.livesRepository.findOneBy({ id: liveId });
+    const tags = await this.tagsService.createTags({ createTagDto });
+    live.title = title;
+    live.tags = tags;
+    return await this.livesRepository.save(live);
+  }
+
+  /**
+   * @todo
+   * 방송 종료 시 endDate, income, replayUrl을 업데이트 하는 로직 작성
+   */
 }
 
 interface ILivesServiceCreateLive {
   createLiveDto: CreateLiveDto;
   userId: string;
+}
+
+interface ILivesServiceUpdateLive {
+  updateLiveDto: UpdateLiveDto;
+  liveId: string;
 }
