a825577c63c1a4e3961b22e41ff7073c71020cd8
cooderyg <ygk9604@naver.com>
2023-08-18 20:38:02 +0900

    feat: refresh strategy refreshToken not found error, cookie parser 리프레쉬 토큰 없을 때 에러 추가, 쿠키파서 추가

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
