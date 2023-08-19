dfeb769c7a4105dc4b7000cf317d190b1dde667d
cooderyg <ygk9604@naver.com>
2023-08-19 19:01:23 +0900

    feat: front ejs setting, billing, dashboard, profile, signin, signup, tables page 프론트 ejs 세팅 빌링, 데시보드, 프로필, 로그인, 회원가입 페이지

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +17,9 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useStaticAssets(join(__dirname, '..', 'views/public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
   await app.listen(3000);
 }
 bootstrap();
