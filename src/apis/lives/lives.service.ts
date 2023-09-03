b544e7377c234a253f32e7b6d822fc9651ce2b6d
cooderyg <ygk9604@naver.com>
2023-09-03 16:16:07 +0900

    feat: add liveStartAlert else if else if 추가

--- src/apis/lives/lives.service.ts
+++ src/apis/lives/lives.service.ts
@@ -239,7 +239,7 @@ export class LivesService {
           { removeOnComplete: true, removeOnFail: true },
         );
       }
-    } else {
+    } else if (!userCount) {
       await this.alertsQueue.add(
         'addAlertQueue',
         {
