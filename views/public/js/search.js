9b74a8e3a2f7bed3d55685baec66839b9666c250
cooderyg <ygk9604@naver.com>
2023-09-08 16:08:18 +0900

    feat: elasticsearch front

--- views/public/js/search.js
+++ views/public/js/search.js
@@ -5,20 +5,25 @@ const searchContainerEl = document.querySelector('#search-container');
 const searchTitleEl = document.querySelector('#search-title');
 
 const getData = async () => {
-  const response = await fetch(`/api/lives/search/keywords?keyword=${keyword}`);
+  const response = await fetch(
+    `/api/lives/search/elastic-search?keyword=${keyword}`,
+  );
   const data = await response.json();
+  console.log(data.hits.hits);
   searchTitleEl.innerText = `"${keyword}"`;
-  const temp = data
+  const temp = data.hits.hits
     .map((el) => {
-      const imgTemp = el.thumbnailUrl || '../img/home-decor-1.jpg';
-      const tagsTemp = el.tags
+      const { _source: source } = el;
+      const imgTemp = source.thumbnailUrl || '../img/home-decor-1.jpg';
+      const tagsTemp = source.tags
+        .split(',')
         .map((tag) => {
-          return `<li>${tag.name}</li>`;
+          return `<li>${tag}</li>`;
         })
         .join('');
       return `
     <li>
-        <a href="/streaming/${el.id}">
+        <a href="/streaming/${source.id}">
             <div class="img-container">
                 <img
                 src=${imgTemp}
@@ -26,12 +31,10 @@ const getData = async () => {
                 />
             </div>
             <div class="desc-container">
-                <h4 class="mb-0 text-lg">${el.channel.name}</h4>
-                <p class="mb-0">${
-                  el.channel?.categories[0] || '카테고리 없음'
-                }</p>
+                <h4 class="mb-0 text-lg">${source.channel_name}</h4>
+                <p class="mb-0">${source?.category_name || '카테고리 없음'}</p>
                 <p class="mb-0">시청자 수</p>
-                <p class="mb-0">${el.title}</p>
+                <p class="mb-0">${source.title}</p>
                 <ul class="tag-container px-0">
                     ${tagsTemp}
                 </ul>
